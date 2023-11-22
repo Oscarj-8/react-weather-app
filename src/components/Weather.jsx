8b1136586543ff30923d7634c843513803e53ac1
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-22 17:04:48 +0300

    Last commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import cloudy from "../../public/cloudy.png";
 
 function Weather() {
   const [city, setCity] = useState("");
@@ -24,22 +25,32 @@ function Weather() {
 
   return (
     <div className="container">
-      <h1>Weather App</h1>
-      <input
-        type="text"
-        placeholder="Enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-      />
-      <button onClick={fetchWeather}>Get Weather</button>
+      <div className="top-section">
+        <img src={cloudy} alt="Weather image" />
+        <h1>Weather App</h1>
+      </div>
+      <div className="input-section">
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button onClick={fetchWeather}>Get Weather</button>
+      </div>
 
       {weather && (
-        <div>
-          <h2>
-            {weather.name}, {weather.sys.country}
-          </h2>
-          <p>{weather.weather[0].description}</p>
-          <p>Temprature: {weather.main.temp} &deg;C </p>
+        <div className="wrapper">
+          <p>
+            <span>City, Country:</span> {weather.name}, {weather.sys.country}
+          </p>
+
+          <p>
+            <span>Weather Desription:</span> {weather.weather[0].description}
+          </p>
+          <p>
+            <span>Temprature:</span> {weather.main.temp} &deg;C{" "}
+          </p>
         </div>
       )}
       {error && <p>{error}</p>}
