53e71244207cbb4c2ffc4c93f369f5b2cfe5f74f
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-11-30 14:38:17 +0300

    Add react-rain-animation lirary

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -9,6 +9,7 @@ const WeatherCard = ({ data }) => {
   const cardStyle = {
     backgroundColor: " #1e90ff",
     borderRadius: "1.4em",
+    boxShadow: "5px 5px 20px #1e90ff",
   };
 
   const CardHeader = {
